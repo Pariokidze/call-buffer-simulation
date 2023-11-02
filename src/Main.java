8c1b7b7bae2816cf6acec30399150d85f6567ec0
Edgaras <edgaras.pariokas@gmail.com>
2023-11-02 19:13:30 +0200

    padaryta 1 2 eile

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Simulation simulation = new Simulation();
-        for (int i=0; i<7; i++)
+        for (int i=0; i<11; i++)
         simulation.generate(new Info());
+        for (int i=0; i<3; i++)
         simulation.serve(new Info());
+
         simulation.printQueue();
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -7,16 +7,26 @@ import java.util.Scanner;
 
 public class Simulation {
     int lastVip = -1;
+    int lastVip2 = -1;
+    boolean SecondQueue = false;
+    boolean ThirdQueue = false;
+
+    int lastVip3 = -1;
     String comment;
     Deque<Info> eile = new ArrayDeque<>();
+    Deque<Info> eile2 = new ArrayDeque<>();
+    Deque<Info> eile3 = new ArrayDeque<>();
 
     boolean CheckVip(Info eile) {
         return eile.isVip;
     }
 
+
     void generate(Info client) throws FileNotFoundException {
         client.enterData();
-        if (CheckVip(client)) {
+        if(eile.size() == 10)
+            SecondQueue = true;
+        if (CheckVip(client) && eile.size() < 10 && !SecondQueue) { // 1 eile
             if (lastVip == -1) {
                 eile.addFirst(client);
                 lastVip = 0;
@@ -35,26 +45,81 @@ public class Simulation {
                 }
                 lastVip++;
         } }
-            else {
+            else if (eile.size() < 10 && !SecondQueue) // 1 eile
+            {
             eile.addLast(client);
+            }
+           else if (eile.size() == 10 & lastVip>0 && SecondQueue) {  // 2 eile
+            Deque<Info> tempDeque = new ArrayDeque<>();
+            for (int i = 0; i <= lastVip ; i++) {
+                tempDeque.addFirst(eile.pollFirst());
+            }
+            int size = tempDeque.size();
+            eile2.addFirst(tempDeque.pollFirst());
+            lastVip--;
+            size--;
+            lastVip2 = 0;
+            for (int i = 0; i < size; i++)
+            {
+                eile.addFirst(tempDeque.pollFirst());
+            }
+            if(!eile.peekLast().isVip)
+            eile2.addLast(eile.pollLast());
         }
-    }
+           else if (SecondQueue){
 
+                if (CheckVip(client) && eile2.size() < 10) {
+                    if (lastVip2 == -1) {
+                        eile2.addFirst(client);
+                        lastVip2 = 0;
+                    }
+                    else {
 
+                        Deque<Info> tempDeque = new ArrayDeque<>();
+                        for (int i = 0; i <= lastVip2 ; i++) {
+                            tempDeque.addFirst(eile2.pollFirst());
+                        }
+                        tempDeque.addFirst(client);
+                        int size = tempDeque.size();
+                        for (int i = 0; i < size; i++)
+                        {
+                            eile2.addFirst(tempDeque.pollFirst());
+                        }
+                        lastVip2++;
+                    } }
+                else
+                {
 
-
+                    eile2.addLast(client);
+                }
+            }}
     void serve(Info client) throws IOException {
+       if (eile.size()>0)
+       {
+           System.out.println("1 eileje Nurodykite komentarus/priezasti del skambucio: ");
+           Scanner scanner = new Scanner(System.in);
+           comment = scanner.nextLine();
+           eile.removeFirst();
+       }
+       if (eile2.size()>0)
+       {
+           System.out.println("2 eileje Nurodykite komentarus/priezasti del skambucio: ");
+           Scanner scanner = new Scanner(System.in);
+           comment = scanner.nextLine();
+           eile2.removeFirst();
+       }
 
-        System.out.println("Nurodykite komentarus/priezasti del skambucio: ");
-        Scanner scanner = new Scanner(System.in);
-        comment = scanner.nextLine();
-        eile.removeFirst();
-
-    }
+       if (eile2.size()== 0)
+           SecondQueue = false;
+       }
 
     void printQueue() {
         System.out.println("Current Queue:");
         for (Info client : eile) {
+            System.out.println(client.toString());
+        }
+        System.out.println("Second Queue:");
+        for (Info client : eile2) {
             System.out.println(client.toString());
         }
     }
